bc4b3fbe298785f45282906301ddc912f5646ff6
XiMiK <ximik53@yandex.ru>
2019-06-05 21:44:53 +0300

    idk

--- src/main/java/com/example/CompanyRepository.java
+++ src/main/java/com/example/CompanyRepository.java
@@ -1,9 +1,17 @@
 package com.example;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
-
+    @Query(
+            name = "search",
+            value = "SELECT id, name FROM company WHERE ST_Contains(ST_GeomFromText(?), company.geometry)",
+            nativeQuery = true)
+    Collection<Company> findAllPoly(String name);
 }
+
--- src/main/java/com/example/Control.java
+++ src/main/java/com/example/Control.java
@@ -1,9 +1,12 @@
 package com.example;
 
+import java.util.Collection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -13,14 +16,24 @@ class Control {
 
     }
     CompanyService service;
+
 @Autowired
+
     public Control(CompanyService repository) {
         this.service = repository;
-    }
 
-    @GetMapping("/")
+    }
+    @GetMapping("/search")
     List<Company> find() {
         return service.findAll();
+
+    }
+
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    List<Company> findPoly(String name) {
+        return service.findAll();
+
+    }
     }
 
 }
